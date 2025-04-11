72c9ba02a3495a6b74c9d934f86c92ac673c27aa
daniel-lib <danieltalems@gmail.com>
2025-04-11 20:58:12 +0300

    feat: authentication - added user registration & login using jwt

--- src/main/java/com/medacare/backend/model/User.java
+++ src/main/java/com/medacare/backend/model/User.java
@@ -3,13 +3,20 @@ package com.medacare.backend.model;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,6 +25,9 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -26,24 +36,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public class User implements Serializable{
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // private String name;
+   
+    @Size(min = 3, max = 100, message = "First name must be between 3 and 100 characters")
+    @NotBlank(message = "First name is required")
     private String firstName;
+    @Size(min = 3, max = 100, message = "Last name must be between 3 and 100 characters")
+    @NotBlank(message = "Last name is required")
     private String lastName;
+    @Column(unique = true)
+    @NotBlank(message = "Email is required")
     private String email;
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    @NotBlank(message = "Password is required")
+    @JsonIgnore
     private String password;
+    
+    private Boolean firstLogin;
 
-    @ManyToOne()
-    @JoinColumn(name = "user_group")
-    private UserGroup userGroup;
-
-    // private String phoneNumber;
+    
     private String gender;
+
+    
+    @NotNull(message = "User Registration Origin is required")
+    @Enumerated(EnumType.STRING)
+    private UserOrigin origin;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
@@ -55,4 +80,52 @@ public class User implements Serializable{
     private LocalDate updatedOn;
 
     private boolean active = true;
+
+    public enum UserOrigin {
+        SELF_REGISTERED,   
+        ORGANIZATION_CREATED,
+        ADMIN_CREATED
+    }
+
+    public enum Role {
+        USER,
+        ADMIN,
+        ORGANIZATION,
+        PHYSICIAN,
+        PATIENT
+    }
+
+     @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/medacare/backend/repository/UserRepository.java
+++ src/main/java/com/medacare/backend/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package com.medacare.backend.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.medacare.backend.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
+
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 
 }
