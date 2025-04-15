6ca191d5ddf24f735ec984eb2675f9b2f6e4d421
daniel-lib <danieltalems@gmail.com>
2025-04-15 15:54:26 +0300

    Fix: Regex pattern based based password & email validation

--- src/main/java/com/medacare/backend/dto/RegisterUserDto.java
+++ src/main/java/com/medacare/backend/dto/RegisterUserDto.java
@@ -9,6 +9,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -16,10 +17,19 @@ import lombok.Data;
 public class RegisterUserDto {
     @Column(unique = true)
     @NotBlank(message = "Email is required")
+    @Pattern(
+    regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+    message = "Invalid email format")
+    @Size(min = 4, max = 100, message = "Email must be between 4 and 100 characters")
+    @Enumerated(EnumType.STRING)
     private String email;
 
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     @NotBlank(message = "Password is required")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character"
+    )
     private String password;
 
     @Size(min = 3, max = 100, message = "First name must be between 3 and 100 characters")
--- src/main/java/com/medacare/backend/model/User.java
+++ src/main/java/com/medacare/backend/model/User.java
@@ -29,6 +29,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -51,15 +52,27 @@ public class User implements UserDetails{
     @Size(min = 3, max = 100, message = "Last name must be between 3 and 100 characters")
     @NotBlank(message = "Last name is required")
     private String lastName;
+
     @Column(unique = true)
     @NotBlank(message = "Email is required")
+    @Pattern(
+    regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+    message = "Invalid email format"
+)
     private String email;
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     @NotBlank(message = "Password is required")
+    @Pattern(
+    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+    message = "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character"
+)
     @JsonIgnore
     private String password;
     
-    private Boolean firstLogin;
+    private Boolean firstLogin = true;
+    private boolean verified = false;
+    @JsonIgnore
+    private String verificationCode;
 
     
     private String gender;
